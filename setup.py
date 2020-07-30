81afb68673d0ed17da74615b50bab2dbe9c676c4
George Tantiras <raratiru@users.noreply.github.com>
2020-07-31 00:57:33 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-quenv",
-    version="0.1.3",
+    version="0.1.4",
     python_requires=">=3.5",
     description=(
         "A django application that gathers the names of all installed packages "
@@ -50,6 +50,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Natural Language :: English",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
